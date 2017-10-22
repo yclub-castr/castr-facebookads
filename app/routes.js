57e80094d0b7fda7907b1d68d84bd74998789f66
Jun Park <jun.parkc@gmail.com>
2017-10-22 17:45:38 +0900

    Start with server and routing structure

--- app/routes.js
+++ app/routes.js
@@ -3,12 +3,17 @@
 'use strict';
 
 const express = require('express');
+const projectRouter = require('./facebookcast/project/project.route');
+const campaignRouter = require('./facebookcast/campaign/campaign.route');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('It\'s alive!!!');
 });
+
+router.use('/project', projectRouter);
+router.use('/campaign', campaignRouter);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoDB = require('./app/db');
 const logger = require('./app/utils').logger();
-const routes = require('./app/routes');
+const adsSdk = require('facebook-ads-sdk');
 
 const app = express();
 const port = process.env.PORT;
@@ -16,14 +16,25 @@ const port = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-Promise.all([mongoDB.connectCastrDB(), mongoDB.connectFBCastDB()])
+mongoDB.isReady()
     .then(() => {
+        logger.debug('Databases are ready');
+
+        app.use('/', (req, res, next) => {
+            logger.debug('# Incoming Request');
+            logger.debug(`# ${req.method} ${req.originalUrl}`);
+            next();
+        });
+
+        const routes = require('./app/routes'); // eslint-disable-line global-require
         app.use('/', routes);
 
         app.listen(port, () => {
+            adsSdk.FacebookAdsApi.init(process.env.ADMIN_SYS_USER_TOKEN);
             logger.debug(`we are live on ${port}`);
         });
     })
     .catch((err) => {
+        logger.error(err);
         logger.error('Application failed to launch');
     });
