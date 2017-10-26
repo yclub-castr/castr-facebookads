239d25f677ddd97cf4112f3ea2300d835275cb3e
Jun Park <jun.parkc@gmail.com>
2017-10-26 16:56:29 +0900

    Implement creative routing

---
+++ app/facebookcast/creative/creative.route.js
@@ -0,0 +1,46 @@
+// app/facebookcast/creative/creative.route.js
+
+'use strict';
+
+const express = require('express');
+const creativeService = require('./creative.service');
+
+const router = express.Router();
+
+router.route('/')
+    .get(async (req, res, next) => {
+        try {
+            const params = {
+                castrLocId: req.query.castrLocId,
+                promotionId: req.query.promotionId,
+            };
+            res.json(await creativeService.getCreatives(params));
+        } catch (err) {
+            next(err);
+        }
+    })
+    .post(async (req, res, next) => {
+        try {
+            const params = {
+                castrLocId: req.body.castrLocId,
+                promotionId: req.body.promotionId,
+            };
+            res.json(await creativeService.createCreative(params));
+        } catch (err) {
+            next(err);
+        }
+    })
+    .delete(async (req, res, next) => {
+        try {
+            const params = {
+                castrLocId: req.body.castrLocId,
+                promotionId: req.body.promotionId,
+            };
+            res.json(await creativeService.deleteCreatives(params));
+        } catch (err) {
+            next(err);
+        }
+    });
+
+
+module.exports = router;
