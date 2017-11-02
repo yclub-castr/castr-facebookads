74449cf3989a847f3f0c904b8a2b1315746c4c83
Jun Park <jun.parkc@gmail.com>
2017-11-02 22:45:33 +0900

    add day in milliseconds constant

--- app/constants.js
+++ app/constants.js
@@ -13,6 +13,8 @@ const locale = {
     us: 'en_US',
 };
 
+const fullDayMilliseconds = 24 * 60 * 60 * 1000;
+
 exports.timezone = (tz) => {
     if (!timezone[tz]) throw new Error('Invalid timezone');
     return timezone[tz];
@@ -21,3 +23,4 @@ exports.locale = (loc) => {
     if (!locale[loc]) throw new Error('Invalid locale: Use either \'us\' or \'kr\' (eg. ?locale=us)');
     return locale[loc];
 };
+exports.fullDayMilliseconds = fullDayMilliseconds;
