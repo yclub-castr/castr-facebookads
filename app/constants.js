74c6e3e9bcadfa195237948c9d7a72c04d993dae
Jun Park <jun.parkc@gmail.com>
2017-11-10 14:54:59 +0900

    Replace timezone map with fb-moment-tzparser

--- app/constants.js
+++ app/constants.js
@@ -1,14 +1,4 @@
-
-const timezone = {
-    kr: 'Asia/Seoul',
-    79: 'Asia/Seoul',
-    us_east: 'America/New_York',
-    7: 'America/New_York',
-    karachi: 'Asia/Karachi',
-    105: 'Asia/Karachi',
-    us_west: '',
-    utc: 'UTC',
-};
+const tzParser = require('fb-moment-tzparser');
 
 const locale = {
     kr: 'ko_KR',
@@ -17,10 +7,7 @@ const locale = {
 
 const fullDayMilliseconds = 24 * 60 * 60 * 1000;
 
-exports.timezone = (tz) => {
-    if (!timezone[tz]) throw new Error('Invalid timezone');
-    return timezone[tz];
-};
+exports.timezone = tz => tzParser.parse(tz);
 exports.locale = (loc) => {
     if (!locale[loc]) throw new Error('Invalid locale: Use either \'us\' or \'kr\' (eg. ?locale=us)');
     return locale[loc];
