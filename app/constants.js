5d03785785083ba2962e79bf611ac5d26af85827
Jun Park <jun.parkc@gmail.com>
2017-11-11 14:22:36 +0900

    Add korean fbRegion -> map plugin mapping

--- app/constants.js
+++ app/constants.js
@@ -5,6 +5,25 @@ const locale = {
     us: 'en_US',
 };
 
+const koreanRegionMap = {
+    Seoul: 'KR-11',
+    Busan: 'KR-26',
+    Daegu: 'KR-27',
+    Incheon: 'KR-28',
+    Gwangju: 'KR-29',
+    Daejeon: 'KR-30',
+    Ulsan: 'KR-31',
+    'Gyeonggi-do': 'KR-41',
+    'Gangwon-do': 'KR-42',
+    'Chungcheongbuk-do': 'KR-43',
+    'Chungcheongnam-do': 'KR-44',
+    'Jeollabuk-do': 'KR-45',
+    'Jeollanam-do': 'KR-46',
+    'Gyeongsangbuk-do': 'KR-47',
+    'Gyeongsangnam-do': 'KR-48',
+    'Jeju-do': 'KR-49',
+};
+
 const fullDayMilliseconds = 24 * 60 * 60 * 1000;
 
 exports.timezone = tz => tzParser.parse(tz);
@@ -13,3 +32,4 @@ exports.locale = (loc) => {
     return locale[loc];
 };
 exports.fullDayMilliseconds = fullDayMilliseconds;
+exports.parseFbRegion = region => koreanRegionMap[region];
