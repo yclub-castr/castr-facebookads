2721ce27b167f516d371c3958b1ca2cbeeb04867
Jun Park <jun.parkc@gmail.com>
2017-11-17 17:35:39 +0900

    Implement promotion insights update instruction

--- app/facebookcast/insight/insight.service.js
+++ app/facebookcast/insight/insight.service.js
@@ -2,16 +2,20 @@
 
 'use strict';
 
-const logger = require('../../utils').logger();
+const utils = require('../../utils');
 const fbRequest = require('../fbapi');
-const moment = require('../../utils').moment();
-const ProjectModel = require('../project/project.model').Model;
+const Project = require('../project/project.model');
 const Insight = require('./insight.model');
 
+const logger = utils.logger();
+const moment = utils.moment();
+
+const ProjectModel = Project.Model;
 const InsightField = Insight.Field;
 const Breakdown = Insight.Breakdown;
 const DatePreset = Insight.DatePreset;
 const Formatter = Insight.Formatter;
+const InsightModel = Insight.Model;
 
 const breakdowns = {
     platform: Breakdown.publisher_platform,
@@ -42,7 +46,7 @@ class InsightService {
             if (!dateRange) {
                 logger.debug('Preparing default \'date_preset\' parameter...');
                 insightParams.date_preset = DatePreset.last_28d;
-                insightParams.time_increment = 1;
+                // insightParams.time_increment = 1;
             } else {
                 logger.debug('Validating \'dateRange\' parameter...');
                 const dates = dateRange.split(',').map(date => moment(date).format('YYYY-MM-DD'));
@@ -52,7 +56,7 @@ class InsightService {
                     throw new Error(`Invalid date range: endDate (${end.format('L')}) cannot be earlier than startDate (${start.format('L')})`);
                 }
                 insightParams.time_range = { since: start, until: end };
-                insightParams.time_increment = 1;
+                // insightParams.time_increment = 1;
             }
 
             // Preparing filtering param
@@ -62,6 +66,15 @@ class InsightService {
             if (castrLocId) adlabels.push(`"${castrLocId}"`);
             if (promotionId) adlabels.push(`"${promotionId}"`);
             insightParams.filtering = `[ {"field": "campaign.adlabels","operator": "ALL","value": [${adlabels.join()}] } ]`;
+
+            const platformReport = await InsightModel.find({
+                // $and:[
+                //     date: {$gte:}
+                // ]
+                castrBizId: castrBizId,
+                castrLocId: castrLocId,
+                promotionId: promotionId,
+            });
 
             let genderAgeResp;
             if (!mock) {
@@ -93,16 +106,6 @@ class InsightService {
                 hourResp = Insight.Mock.hour();
             }
 
-            // let platformResp;
-            // if (!mock) {
-            //     const platformParams = Object.assign({}, insightParams);
-            //     platformParams.breakdowns = breakdowns.platform;
-            //     platformParams.fields = fields.platform;
-            //     platformResp = await fbRequest.get(accountId, 'insights', platformParams);
-            // } else {
-            //     platformResp = Insight.Mock.platform();
-            // }
-
             const demoReport = {
                 impressions: {},
                 clicks: {},
@@ -113,15 +116,116 @@ class InsightService {
             Formatter.region(demoReport, regionResp.data, params.locale);
             Formatter.hour(demoReport, hourResp.data);
 
-            const msg = `Insights returned`;
+            const msg = 'Insights returned';
             logger.debug(msg);
             return {
                 success: true,
                 message: msg,
                 data: {
-                    platformReport: Insight.Mock.platform(accountTimezone),
+                    platformReport: platformReport,
                     demoReport: demoReport,
                 },
+            };
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async updatePromotionInsights(promotionParams, mock) {
+        const insightParams = {
+            breakdowns: breakdowns.platform,
+            fields: fields.platform,
+            data_preset: DatePreset.last_28d,
+            time_increment: 1,
+        };
+        try {
+            const insightsRequests = [];
+            if (!mock) {
+                for (let i = 0; i < promotionParams.length; i++) {
+                    const params = promotionParams[i];
+                    const adlabels = [];
+                    adlabels[0] = `"${params.castrBizId}"`;
+                    adlabels[1] = `"${params.castrLocId}"`;
+                    adlabels[2] = `"${params.promotionId}"`;
+                    insightParams.filtering = `[ {"field": "campaign.adlabels","operator": "ALL","value": [${adlabels.join()}] } ]`;
+                    const insightPromise = fbRequest.get(params.accountId, 'insights', insightParams);
+                    insightsRequests.push(insightPromise.then((fbResponse) => {
+                        params.insights = fbResponse;
+                    }));
+                }
+            } else {
+                for (let i = 0; i < promotionParams.length; i++) {
+                    const params = promotionParams[i];
+                    const insightPromise = Insight.Mock.platform(params.timezone);
+                    insightsRequests.push(insightPromise.then((fbResponse) => {
+                        params.insights = fbResponse;
+                    }));
+                }
+            }
+            const platformResp = await Promise.all(insightsRequests);
+
+            const bulkWrites = [];
+            promotionParams.forEach((params) => {
+                if (!params.insights) return;
+                params.insights.data.forEach((insights) => {
+                    const actions = {};
+                    if (insights.actions) {
+                        insights.actions.forEach((event) => {
+                            if (event.action_type === 'link_click') actions.linkClicks = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_add_payment_info') actions.addPaymentInfo = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_add_to_cart') actions.addToCart = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_add_to_wishlist') actions.addToWishlist = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_complete_registration') actions.completeRegistration = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_initiate_checkout') actions.initiateCheckout = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_lead') actions.lead = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_purchase') actions.purchase = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_search') actions.search = event.value;
+                            else if (event.action_type === 'offsite_conversion.fb_pixel_view_content') actions.viewContent = event.value;
+                        });
+                    }
+                    const dateElems = insights.date_stop.split('-');
+                    const date = moment.tz(params.timezone)
+                        .year(dateElems[0]).month(dateElems[1] - 1).date(dateElems[2])
+                        .hour(0).minute(0).second(0).millisecond(0);
+                    bulkWrites.push({
+                        updateOne: {
+                            filter: {
+                                date: date.toDate(),
+                                castrBizId: params.castrBizId,
+                                castrLocId: params.castrLocId,
+                                promotionId: params.promotionId,
+                                platform: insights.publisher_platform,
+                            },
+                            update: {
+                                spend: insights.spend,
+                                reach: insights.reach,
+                                impressions: insights.impressions,
+                                clicks: insights.clicks,
+                                linkClicks: actions.linkClicks,
+                                purchases: actions.purchase,
+                                addPaymentInfo: actions.addPaymentInfo,
+                                addToCart: actions.addToCart,
+                                addToWishlist: actions.addToWishlist,
+                                completeRegistration: actions.completeRegistration,
+                                initiateCheckout: actions.initiateCheckout,
+                                lead: actions.lead,
+                                search: actions.search,
+                                viewContent: actions.viewContent,
+                            },
+                            upsert: true,
+                        },
+                    });
+                });
+            });
+
+            const bulkWriteResult = await InsightModel.bulkWrite(bulkWrites, { ordered: false });
+
+            const msg = `${platformResp.length} promotions insights updated`;
+            logger.debug(msg);
+            return {
+                success: true,
+                message: msg,
+                data: [],
             };
         } catch (err) {
             throw err;
